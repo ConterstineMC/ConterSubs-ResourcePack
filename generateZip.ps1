d77c2597e801e42f0026f7a312c47f5b79c8bbfd
Doc <nachito94@msn.com>
2024-11-28 11:11:23 -0300

    Ajuste en utilidad

--- generateZip.ps1
+++ generateZip.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path ".\assets", ".\pack.mcmeta", ".\pack.png", ".\README.md" -DestinationPath ".\ConterSubs-ResourcePack.zip"
+Compress-Archive -Path ".\assets", ".\pack.mcmeta", ".\pack.png", ".\README.md" -DestinationPath ".\ConterSubs-ResourcePack.zip" --force
