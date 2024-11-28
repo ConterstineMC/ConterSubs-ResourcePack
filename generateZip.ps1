ad79e9ec4ed3062c8b87e17f5008a4805bff949f
Doc <nachito94@msn.com>
2024-11-28 11:11:55 -0300

    Fix del ajuste anterior

--- generateZip.ps1
+++ generateZip.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path ".\assets", ".\pack.mcmeta", ".\pack.png", ".\README.md" -DestinationPath ".\ConterSubs-ResourcePack.zip" --force
+Compress-Archive -Path ".\assets", ".\pack.mcmeta", ".\pack.png", ".\README.md" -DestinationPath ".\ConterSubs-ResourcePack.zip" -Force
